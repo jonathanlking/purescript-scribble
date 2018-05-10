0ab516afebb1fbae1f3aef8aa286de9d872f8853
Jonathan King <jonathanlking@me.com>
2018-05-10 17:53:30 +0100

    Add two-buyer example

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,7 +1,6 @@
 module Main where
 
 import Control.Parallel
-
 import Control.Apply ((<*>))
 import Control.Coroutine as CR
 import Control.Monad.Aff (Aff, launchAff)
@@ -15,19 +14,99 @@ import DOM.Websocket.WebSocket as WS
 import Data.Argonaut.Core (Json)
 import Data.Symbol (SProxy(SProxy))
 import Data.Tuple (Tuple(..))
-import Prelude (Unit, bind, discard, otherwise, pure, show, ($), (*), (+), (-), (<<<), (<=))
+import Prelude (Unit, bind, discard, otherwise, pure, show, ($), (*), (+), (-), (<<<), (<=), (>), min, unit)
 import Data.Functor ((<$>))
 import Scribble.Coroutine as SC
 import Scribble.FSM (Protocol(..), Role(..))
 import Scribble.Protocol.Arithmetic (Arithmetic, C1, Client, S1, S8, Server)
+import Scribble.Protocol.Multiparty.TwoBuyer
 import Scribble.Transport.WebSocket (WebSocket)
 import Type.Proxy (Proxy(..))
 
-main :: _ 
 main = launchAff $ do
+  -- Run the Arithmetic example
   sequential $
-    Tuple <$> parallel (prog 15)
-          <*> parallel runServer 
+    (\ _ _ -> unit)
+      <$> parallel (prog 15)
+      <*> parallel runServer 
+  -- Run the Two-Buyer example
+  sequential $
+    (\ _ _ _ -> unit) 
+      <$> parallel buyer1
+      <*> parallel buyer2
+      <*> parallel seller
+
+-- Examples for the Two-Buyer protocol
+
+buyer1 :: forall eff. Aff (dom :: DOM, avar :: AVAR | eff) Unit
+buyer1 = SC.multiSession
+  (Proxy :: Proxy WebSocket)
+  (WS.URL $ "ws://127.0.0.1:9160") 
+  (Protocol :: Protocol TwoBuyer)
+  (Tuple (Role :: Role Buyer1) (SC.Identifier "Jonathan"))
+  {"Buyer2": SC.Identifier "Nick", "Seller": SC.Identifier "Nobuko"}
+  (\c -> do
+    c <- lift $ SC.send c (Book "War and Peace")
+    (Tuple (Quote price) c) <- SC.receive c
+    -- Buyer1 can contribute a maximum of £10
+    let cont = min 10 price
+    c <- lift $ SC.send c (Contribution cont)
+    SC.choice c { agree: \c -> do
+      (Tuple Agree c) <- SC.receive c
+      lift $ SC.send c (Transfer cont) 
+                , quit: \c -> do
+      (Tuple Quit c) <- SC.receive c
+      pure c
+                }
+  )
+
+buyer2 :: forall eff. Aff (dom :: DOM, avar :: AVAR | eff) Unit
+buyer2 = SC.multiSession
+  (Proxy :: Proxy WebSocket)
+  (WS.URL $ "ws://127.0.0.1:9160") 
+  (Protocol :: Protocol TwoBuyer)
+  (Tuple (Role :: Role Buyer2) (SC.Identifier "Nick"))
+  {"Buyer1": SC.Identifier "Jonathan", "Seller": SC.Identifier "Nobuko"}
+  (\c -> do 
+    (Tuple (Quote price) c) <- SC.receive c
+    (Tuple (Contribution amount) c) <- SC.receive c
+    let cont = price - amount -- The amount Buyer2 has to contribute
+    if cont > 20 -- Buyer 2 will contribute up to £20
+      then do
+        c <- lift $ SC.select c (SProxy :: SProxy "quit")
+        c <- lift $ SC.send c Quit
+        lift $ SC.send c Quit
+      else do
+        c <- lift $ SC.select c (SProxy :: SProxy "agree")
+        c <- lift $ SC.send c Agree
+        c <- lift $ SC.send c Agree
+        lift $ SC.send c (Transfer cont)
+  )
+
+seller :: forall eff. Aff (dom :: DOM, avar :: AVAR | eff) Unit
+seller = SC.multiSession
+  (Proxy :: Proxy WebSocket)
+  (WS.URL $ "ws://127.0.0.1:9160") 
+  (Protocol :: Protocol TwoBuyer)
+  (Tuple (Role :: Role Seller) (SC.Identifier "Nobuko"))
+  {"Buyer1": SC.Identifier "Jonathan", "Buyer2": SC.Identifier "Nick"}
+  (\c -> do
+    (Tuple (Book name) c) <- SC.receive c
+    let quote = 30
+    c <- lift $ SC.send c (Quote quote)
+    c <- lift $ SC.send c (Quote quote)
+    SC.choice c { agree: \c -> do
+      (Tuple Agree c) <- SC.receive c
+      (Tuple (Transfer t1) c) <- SC.receive c
+      (Tuple (Transfer t1) c) <- SC.receive c
+      pure c
+                , quit: \c -> do
+      (Tuple Quit c) <- SC.receive c
+      pure c
+                }
+  )
+
+-- Examples for the Arithmetic protocol
 
 prog :: forall eff. Int -> Aff (dom :: DOM, avar :: AVAR, console :: CONSOLE | eff) Unit
 prog n 
